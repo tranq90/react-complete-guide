30641165c67235dae717ede956b9b64cae0d96c2
Quan Tran <tran.q90@gmail.com>
2021-04-14 19:45:59 -0700

    adding chart and chartbar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesList from './ExpensesList';
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "../Expenses/ExpenseFilter";
+import ExpensesChart from '../Expenses/ExpensesChart';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,7 +30,7 @@ function ExpenseForm(props) {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
